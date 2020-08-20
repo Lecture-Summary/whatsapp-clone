8a9bf175c6a1eab22082b6b41d70889c8f888919
hojin9622 <kiss0104040@gmail.com>
2020-08-20 20:05:45 +0900

    Create Sidebar Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-        {/* Sidebar */}
+        <Sidebar />
         {/* Chat */}
       </div>
     </div>
