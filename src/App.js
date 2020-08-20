8d438d13050391fb94ac7898ea037d061eb3d7d4
hojin9622 <kiss0104040@gmail.com>
2020-08-20 23:08:57 +0900

    Create Login Style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Login from "./Login";
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
 
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import "./Login.css";
+
+function Login() {
+  const signIn = () => {};
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/150px-WhatsApp.svg.png"
+          alt=""
+        />
+        <div className="login__text">
+          <h1>Sign in to WhatsApp</h1>
+        </div>
+
+        <Button type="submit" onClick={signIn}>
+          Sign In With Google
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
