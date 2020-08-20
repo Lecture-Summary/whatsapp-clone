eec8f6c0a9b61e59e71c2c8c568840b541259aa2
hojin9622 <kiss0104040@gmail.com>
2020-08-20 21:34:22 +0900

    Create Chat Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 import "./App.css";
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        {/* Chat */}
+        <Chat />
       </div>
     </div>
   );
---
+++ src/Chat.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import "./Chat.css";
+
+function Chat() {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seend at ...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body"></div>
+
+      <div className="chat__footer"></div>
+    </div>
+  );
+}
+
+export default Chat;
