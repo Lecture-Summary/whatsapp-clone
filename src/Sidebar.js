062f32ed915e84d007a4b4458403df19515e18c8
hojin9622 <kiss0104040@gmail.com>
2020-08-20 22:25:33 +0900

    Upload Chat Room to DB

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SidebarChat from "./SidebarChat";
 import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import db from "./firebase";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) => {
+      setRooms(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -34,9 +48,9 @@ function Sidebar() {
 
       <div className="sidebar__chats">
         <SidebarChat addNewChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
+        {rooms.map((room) => (
+          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+        ))}
       </div>
     </div>
   );
