c8abd4b4fa73eacc69fec6860b03a04f495005bb
hojin9622 <kiss0104040@gmail.com>
2020-08-20 19:44:03 +0900

    Create Body Style

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a WhatsApp Clone!</h1>
-
       <div className="app__body">
         {/* Sidebar */}
         {/* Chat */}
